1cb5ad87e27823d4ba3f0429a2ec2c9770c60a71
ori halevi <orihalevi98@gmail.com>
2025-06-25 05:21:11 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace RdpScopeTogglerToastListener
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            bool createdNew;
+            using (var mutex = new Mutex(true, "RdpScopeTogglerToastMutex", out createdNew))
+            {
+                if (!createdNew)
+                    return; // Already running → exiting
+
+                // NotifyIcon — Taskbar icon
+                NotifyIcon notifyIcon = new()
+                {
+                    Icon = new System.Drawing.Icon("Assets/remote-desktop.ico"),
+                    Visible = true,
+                    Text = "Rdp Scope Toggler Listener"
+                };
+
+                // Listening to goodbye
+                ToastNotificationManagerCompat.OnActivated += toastArgs => { };
+
+                string path = @"C:\ProgramData\RdpScopeToggler";
+                string fileName = "ToastMessage.txt";
+                string fullFilePath = Path.Combine(path, fileName);
+
+                Directory.CreateDirectory(path);
+
+                var watcher = new FileSystemWatcher(path, fileName)
+                {
+                    NotifyFilter = NotifyFilters.LastWrite
+                };
+
+                watcher.Changed += (s, e) =>
+                {
+                    try
+                    {
+                        Thread.Sleep(100);
+                        string content = File.ReadAllText(fullFilePath).Trim();
+
+                        if (!string.IsNullOrWhiteSpace(content))
+                        {
+                            new ToastContentBuilder()
+                                .AddText("Rdp Scope Toggler")
+                                .AddText(content)
+                                .Show();
+                        }
+                    }
+                    catch { }
+                };
+
+                watcher.EnableRaisingEvents = true;
+
+                // Keeps the process active without a window
+                Application.Run();
+            }
+        }
+    }
+}
